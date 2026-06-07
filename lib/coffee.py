d010bc1ae3ed8f3d1e45b3970cefe49b6dde1043
kagika <r.m.kagika@gmail.com>
2026-06-07 14:30:31 +0300

    Lab submsiion and completed coffee.py and book.py

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    pass
+    def __init__(self,size,price):
+        self.size = size
+        self.price = price
+    
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size(self,serving_size):
+        if serving_size not in ["Small", "Medium", "Large"]:
+            print("size must be Small, Medium, or Large")
+        self._size = serving_size
+        
+    
+    def tip(self):
+        print("This coffee is great, here’s a tip!")
+        self.price+=1
+
+coffee = Coffee("Small",30)
